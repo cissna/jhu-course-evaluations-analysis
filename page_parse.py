827e517f40a09d880ac370c8878cd45cb4a2bef6
Isaac Cissna <isaaccissna@gmail.com>
2025-03-29 23:23:23 -0400

    added string parsing for SpecificClassScraper in __init__

--- page_parse.py
+++ page_parse.py
@@ -2,7 +2,72 @@
 Uses classes for classes, where a class a scraper method that gets the relevant data from https://asen-jhu.evaluationkit.com/Report/Public/Results
 """
 
+import re
 
+
+def parse_period(period_string: str) -> str:
+    """Processes period_string to ensure consistency
+
+    Args:
+        period_string (str): unparsed string
+
+    Raises:
+        ValueError: It should fit into one of the 4 periods, if not this is raised.
+
+    Returns:
+        str: 'SP'--spring, 'FA'--fall, 'IN'--intersession, 'SU'--summer
+    """
+    period_string = period_string.lower().strip()
+    if period_string == 'sp' or period_string == 'spring':
+        return 'SP'
+    if period_string == 'fa' or period_string == 'fall':
+        return 'FA'
+    if period_string == 'in' or period_string == 'intersession':
+        return 'IN'
+    if period_string == 'su' or period_string == 'summer':
+        return 'SU'
+    raise ValueError(f'Period String, "{period_string}" should be spring, fall, intersession, or summer')
+
+
+def parse_year(year_string: str) -> int:
+    """Processes year_string to ensure consistency
+
+    Args:
+        year_string (str): unparsed string
+
+    Raises:
+        ValueError: It should be a valid year, if not this is raised.
+
+    Returns:
+        int: integer of year (00 <= year < 100) 
+    """
+    year_string = year_string.strip()
+    if re.match(r"^\d{4}$", year_string):
+        return int(year_string) - 2000
+    else:
+        if re.match(r"^\d{1,2}$", year_string):
+            return int(year_string)
+        else:
+            raise ValueError(f'Year, "{year_string}" is not valid')
+
+
+
+def parse_section(section_string: str) -> int:
+    """Processes section_string to ensure consistency
+
+    Args:
+        section_string (str): unparsed string
+
+    Raises:
+        ValueError: It should be a 1-2 digit integer, if not this is raised.
+
+    Returns:
+        int: integer of section (00 <= section < 100) 
+    """
+    if re.match(r"^\d{1,2}$", section_string.strip()):
+        return int(section_string)
+    else:
+        raise ValueError(f'Section, "{section_string}" is not valid')
 
 
 class SpecificClassScraper():
@@ -10,7 +75,15 @@ class SpecificClassScraper():
     holds information of a specific class, and has a method to scrape the data for that class
     """
     def __init__(self, class_code: str, period_string: str, year_string: str, section_string: str):
-        pass  # process strings to make a properly formatted string that matches JHU's formatting
+        pattern = r'^[a-z]{2}\.\d{3}\.\d{3}$'
+        if not re.match(pattern, class_code.lower().strip()):
+            raise ValueError(f"{class_code} is a invalid class code (should be XX.###.###)")
+        
+        period = parse_period(period_string)
+        year = parse_year(year_string)
+        section = parse_section(section_string)
+        # at this point, the variables we care about are year, period, section, and class_code, here's how we use them:
+        specific_class_code = f'{class_code}.{section:02}.{period}{year:02}'
 
     def scrape_pdf(self):
         pass
