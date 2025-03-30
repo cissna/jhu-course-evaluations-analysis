53b555eb5a510adc1e0bc349fd4244c08a9d198b
Isaac Cissna <isaaccissna@gmail.com>
2025-03-29 23:17:58 -0400

    added page_parse.py and added some basic skeletons

---
+++ page_parse.py
@@ -0,0 +1,20 @@
+"""
+Uses classes for classes, where a class a scraper method that gets the relevant data from https://asen-jhu.evaluationkit.com/Report/Public/Results
+"""
+
+
+
+
+class SpecificClassScraper():
+    """
+    holds information of a specific class, and has a method to scrape the data for that class
+    """
+    def __init__(self, class_code: str, period_string: str, year_string: str, section_string: str):
+        pass  # process strings to make a properly formatted string that matches JHU's formatting
+
+    def scrape_pdf(self):
+        pass
+
+            
+
+        
