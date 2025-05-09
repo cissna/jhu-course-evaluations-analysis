1d41a84c1618398ed3e956fe19e9429a17e1085e
Isaac Cissna <isaaccissna@gmail.com>
2025-05-09 02:40:53 -0400

    comment clarifying how manage_failed_downloads works well with above implementation of IN/SU
    specifically it's full stop after failures:

--- page_parse.py
+++ page_parse.py
@@ -409,7 +409,8 @@ class GeneralClassScraper():
                             break  # default behavior is we stop searching once we don't find a value
                     elif result is False:
                         self.cache.mark_failed(s.specific_class_code)
-                        break
+                        break  # manage_failed_downloads.py already deals with this well,
+                               # so if it fails we fully stop this period, continue onwards in solve_simple_failures()
                     
                     self.cache = s.parse_pdf()
 
